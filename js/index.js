e39a6e5007232eb9a8fefd3e1daa9e07f1c535c8
Tndevfactory <tndevfactory@gmail.com>
2021-11-28 11:46:03 +0100

    init three

---
+++ js/index.js
@@ -0,0 +1,38 @@
+import * as THREE from '../node_modules/three/build/three.module.js';
+
+let camera, scene, renderer;
+let geometry, material, mesh;
+
+init();
+
+function init() {
+
+	camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
+	camera.position.z = 1;
+	scene = new THREE.Scene();
+
+
+  //box
+	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+	material = new THREE.MeshNormalMaterial();
+	mesh = new THREE.Mesh( geometry, material );
+  scene.add( mesh );
+
+  const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+  scene.add( light );
+
+	renderer = new THREE.WebGLRenderer( { antialias: true } );
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setAnimationLoop( animation );
+	document.body.appendChild( renderer.domElement );
+
+}
+
+function animation( time ) {
+
+	mesh.rotation.x = time / 2000;
+	mesh.rotation.y = time / 1000;
+
+	renderer.render( scene, camera );
+
+}
